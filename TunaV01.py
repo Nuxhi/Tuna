d3715f784080e2ec09be0b15cfb48f99b7968da8
Nuxhi <78807626+Nuxhi@users.noreply.github.com>
2022-12-30 12:46:58 +0100

    Update TunaV01.py

--- TunaV01.py
+++ TunaV01.py
@@ -2,54 +2,53 @@ import os
 import getpass
 from requests_html import HTMLSession
 
+# Tuna express : configuration
 
-# Tuna configuration
-
-exp_version = 'TunaV01.py'
-version = 'TunaV1'
-last_version = 'TunaV2.py'
+exp_version = 'TunaV0.py'
+version = 'TunaV01'
+last_version = 'TunaV1.py'
+filetype = 'py'
 
 link = "https://github.com/Nuxhi" #Lien de votre profile GitHub 
-link_update = "https://github.com/Nuxhi/Tuna/raw/main/"+last_version #Lien de téléchargement directe de votre fichier sur GitHub
+
+if filetype == 'py':
+    link_update = "https://github.com/Nuxhi/Tuna/archive/refs/heads/main.zip"
+
+if filetype == 'exe':
+    link_update = "https://github.com/Nuxhi/Tuna/raw/main/"+last_version #Lien de téléchargement directe de votre fichier sur GitHub
 
 path = os.getcwd() # assign folder path
 username = getpass.getuser() # assign user os
 
 
 def tuna():
-
+    os.chdir(path)
+    print(path) 
+    
     print('Please wait, checking for updates. 1/2')
-    os.chdir(path) # peut importe l'emplacement de votre fichier, Tuna se met la ou vous l'avez executer.
-    try:
-        os.remove(exp_version)
-    except:
-        print('')
-
-    #Ouverture de la session HTML, début de la procésdure 
-
-    print('Please wait, checking for updates. 2/2')
     s = HTMLSession()
     rqt = s.get(link)
     
     #Vérification de la disponibilité des serveurs.
     if rqt.ok:
         print('[Console] : Serveur disponible !')
+        try:
+            title = rqt.html.find('.p-org')[0].text
+        except:
+            title = ('UKN')
     else:
         print('[Console] : Serveur indisponible ! ', rqt)
    
-    try:
-        title = rqt.html.find('.p-org')[0].text
-    except:
-        title = ('UKN')
-
     if title != version:
+        print('Please wait, checking for updates. 2/2')
         print('update disponible !\nVersion : ',title,'disponible,\nTéléchargement en cours...')
+        print(title, link_update)
         r_upt = s.get(link_update, allow_redirects=True)
         open(last_version, 'wb').write(r_upt.content)
-
         os.startfile(last_version) 
-        print('start', last_version)
         exit
 
     else:
         print('Aucune update disponible !')
+
+tuna()
